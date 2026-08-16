7565503abeabc3ba8830d90436c25e6a099874fb
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-08-16 14:23:42 -0700

    Remove unused `.meta` folders

--- tests/success-if-all-tests-pass/.meta/Example.vb
+++
@@ -1,5 +0,0 @@
-Public Module Leap
-    Public Function IsLeapYear(ByVal year As Integer) As Boolean
-        Return year Mod 400 = 0 OrElse (year Mod 100 <> 0 AndAlso year Mod 4 = 0)
-    End Function
-End Module
